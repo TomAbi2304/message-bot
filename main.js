f1c62a597e950c2fb40308d0023db11a2cf47174
TomAbi2304 <157975833+TomAbi2304@users.noreply.github.com>
2024-02-16 20:36:38 +0100

    main.js

---
+++ main.js
@@ -0,0 +1,131 @@
+const discord = require('discord.js')
+
+const client = new discord.Client({
+    intents: [
+        discord.GatewayIntentBits.GuildMessages,
+        discord.GatewayIntentBits.GuildMembers,
+        discord.GatewayIntentBits.GuildMessageReactions,
+        discord.GatewayIntentBits.Guilds,
+        discord.GatewayIntentBits.GuildPresences,
+    ]
+})
+
+function embedcreator(path) {
+    let config = require('./config')
+    const { EmbedBuilder } = require('discord.js');
+
+    // get the config path
+    const args = path.split('.')
+
+    // get the config
+    let conf = config
+
+    // get item from config
+    args.forEach(argument => {
+        conf = conf[argument]
+    });
+
+    // create a new EmbedBuilder
+    const embed = new EmbedBuilder()
+        .setColor(conf.color || config.bot.color)
+        .setTitle(conf.title)
+        .setDescription(conf.description)
+
+    // check if the author iconURL and Footer iconURL is a url
+    if (conf.author?.iconURL && !conf.author.iconURL.startsWith('http')) conf.author.iconURL = null
+    if (conf.footer?.iconURL && !conf.footer.iconURL.startsWith('http')) conf.footer.iconURL = null
+    
+    // set the author and footer
+    if (conf.footer?.text) embed.setFooter(conf.footer)
+    if (conf.author?.name) embed.setAuthor(conf.author)
+
+    // set the image, banner, timestamp and url
+    if (conf.image) embed.setThumbnail(conf.image)
+    if (conf.banner) embed.setImage(conf.banner)
+    if (conf.timestamp) embed.setTimestamp()
+    if (conf.url) embed.setURL(conf.url)
+
+    // add fields
+    if (conf.fields) conf.fields.forEach(filed => {
+        embed.addFields(filed)
+    });
+
+    // return the embed
+    return embed
+}
+
+const config = require('./config.js')
+
+client.on('ready', () => {
+    console.log('Logged in as ' + client.user.tag + '!')
+    if (!config.bot.activitys) return
+    if (!config.bot.intervall) return
+    if (!config.bot.enabled) return
+    setInterval(() => {
+        let i = 0
+        const activity = config.bot.activitys[i]
+        client.user.setStatus(activity.status)
+        client.user.setActivity({ name: activity.name, type: activity.type })
+        i++
+    }, config.bot.intervall)
+})
+
+client.on('guildMemberAdd', async (member) => {
+    const channel = member.guild.channels.cache.get(config.messages.welcome.channel)
+    if (!channel) return
+
+    const embed = embedcreator('messages.welcome')
+        .setDescription(config.messages.welcome.description
+            .replace('{userping}', member.toString())
+            .replace('{username}', member.user.username)
+            .replace('{displayname}', member.displayName)
+            .replace('{ammountmember}', member.guild.memberCount)
+        )
+
+    if (config.messages.welcome.dm) try { member.send({ embeds: [embed] }) } catch (error) {}
+
+    channel.send({ embeds: [embed] })
+
+    config.messages.welcome.roles.forEach(role => {
+        const dcrole = member.guild.roles.cache.get(role)
+        if (!dcrole) return
+        member.roles.add(dcrole)
+    })
+})
+
+client.on('guildMemberRemove', async (member) => {
+    const channel = member.guild.channels.cache.get(config.messages.goodbye.channel)
+    if (!channel) return
+
+    const embed = embedcreator('messages.goodbye')
+        .setDescription(config.messages.goodbye.description
+            .replace('{userping}', member.toString())
+            .replace('{username}', member.user.username)
+            .replace('{displayname}', member.displayName)
+            .replace('{ammountmember}', member.guild.memberCount)
+        )
+    channel.send({ embeds: [embed] })
+})
+
+client.on('guildMemberUpdate', async (oldmember, newmember) => {
+    if (newmember.premiumSinceTimestamp === null) return
+    if (oldmember.premiumSinceTimestamp === newmember.premiumSinceTimestamp) return
+    const channel = newmember.guild.channels.cache.get(config.messages.boost.channel)
+    if (!channel) return
+
+    const embed = embedcreator('messages.boost')
+        .setDescription(config.messages.boost.description
+            .replace('{userping}', newmember.toString())
+            .replace('{username}', newmember.user.username)
+            .replace('{displayname}', newmember.displayName)
+            .replace('{ammountboosts}', newmember.guild.premiumSubscriptionCount)
+        )
+        
+    if (config.messages.boost.dm) try { newmember.send({ embeds: [embed] }) } catch (error) {}
+        
+    const msg = await channel.send({ embeds: [embed] })
+    
+    if (config.messages.boost.reaction.enabled) msg.react(config.messages.boost.reaction.emoji)
+})
+
+client.login(config.bot.token)
